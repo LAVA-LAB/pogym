5989cae11817f2360685edb96f625486bb0b13f5
tdsimao <tdsimao@users.noreply.github.com>
2022-08-19 13:14:16 +0200

    simplifly entry_points

--- pogym/__init__.py
+++ pogym/__init__.py
@@ -1,3 +1,4 @@
 # allow us to create an environment using pogym.make
 from gym.envs.registration import make
+from .envs.pomdp import *
 import pogym.envs
--- pogym/envs/__init__.py
+++ pogym/envs/__init__.py
@@ -2,18 +2,18 @@ from gym.envs.registration import register
 
 register(
     id='CheeseMaze-v0',
-    entry_point='pogym.envs.pomdp.cheesemaze:CheeseMazeEnv',
+    entry_point='pogym:CheeseMazeEnv',
     max_episode_steps=300,
 )
 
 register(
     id='Tiger-v0',
-    entry_point='pogym.envs.pomdp.tiger:TigerEnv',
+    entry_point='pogym:TigerEnv',
     max_episode_steps=300,
 )
 
 register(
     id='Voicemail-v0',
-    entry_point='pogym.envs.pomdp.voicemail:VoicemailEnv',
+    entry_point='pogym:VoicemailEnv',
     max_episode_steps=300,
 )
