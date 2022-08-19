8e2ce404d3d926d93429e77169d924e260a5ef6f
tdsimao <tdsimao@users.noreply.github.com>
2022-08-19 16:06:28 +0200

    use random actions to create the trajectory

--- pogym/tests/utils.py
+++ pogym/tests/utils.py
@@ -1,10 +1,14 @@
 
 
-def get_trajectory(env):
-    observations = [env.reset(seed=0)]
-    truncated = False
-    while not truncated:
-        o, _, _, truncated, _ = env.step(2)
+def get_trajectory(env: Env):
+    o = env.reset(seed=123)
+    env.action_space.seed(123)
+    observations = [o]
+    terminated, truncated = False, False
+
+    while not (truncated or terminated):
+        a = env.action_space.sample()
+        o, _, terminated, truncated, _ = env.step(a)
         observations.append(o)
     return observations
 
